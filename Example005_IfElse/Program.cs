f74951f68302b64e12f3725e6938c5d4fe19b0c4
Igor Pavlenko <ipavlenkoysr13@bk.ru>
2022-03-03 23:51:01 +0300

    Added if/else pair

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "mohammad")
+{
+    Console.WriteLine("Hoorah, it is Mohammad!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
