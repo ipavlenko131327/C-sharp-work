7cb0a51cb0bcc9748633ad9d5cbeb8a2e8232485
Igor Pavlenko <ipavlenkoysr13@bk.ru>
2022-03-03 23:35:00 +0300

    Added Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int number1 = 35;
-int number2 = 52;
+﻿int number1 = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(number1);
+int number2 = new Random().Next(1, 10);
+Console.WriteLine(number2);
 int result = number1+number2;
 Console.WriteLine(result);
 
