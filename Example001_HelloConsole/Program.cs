6ffeb1ef5457abc4ac2b2184ae570c286d6985a4
Igor Pavlenko <ipavlenkoysr13@bk.ru>
2022-04-19 22:40:51 +0300

    Changes done

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("2 Hello, World!");
+﻿int[] array = { 1, 3, 5, 7, 9, 3, 6, 1, 4, 2, 8, 10 };
+void printarray(int[] array)
+{
+    int count = array.Length;
+    
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+
+    }
+    Console.WriteLine();
+} 
+
+void selectionsort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minposition = i;
+
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minposition]) minposition = j;
+        }
+
+        int temp = array[i];
+        array[i] = array[minposition];
+        array[minposition] = temp;
+    }
+}
+
+printarray(array);
+selectionsort(array);
+
+printarray(array);
